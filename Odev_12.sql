9d188e2b66910b6a0899a58b7933c88a938a1bb4
Mehmet Ali YILMAZ <110493375+Ind-Mali@users.noreply.github.com>
2022-08-15 16:52:04 +0300

    Create Odev_12.sql

---
+++ Odev_12.sql
@@ -0,0 +1,25 @@
+--film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+
+select count(title) from film
+where length >(select avg(length) from film);
+
+--film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+select count(*) from film 
+where rental_rate = (select max(rental_rate) from film);
+
+--film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+select title from film 
+where rental_rate = 
+(
+	select min(rental_rate) from film
+)
+and replacement_cost =
+(
+select min(replacement_cost) from film
+);
+
+--payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+select count(payment.customer_id), payment.customer_id, first_name,last_name from customer
+right join payment on customer.customer_id = payment.customer_id
+group by payment.customer_id, first_name, last_name
+order by count(payment.customer_id) desc;
